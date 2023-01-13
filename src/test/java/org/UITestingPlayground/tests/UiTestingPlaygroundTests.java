e067cae951a0f009798416927d73bc1dcec7604b
Hend <hendfouda@gmail.com>
2023-01-13 16:56:48 +0200

    test

--- src/test/java/org/UITestingPlayground/tests/UiTestingPlaygroundTests.java
+++ src/test/java/org/UITestingPlayground/tests/UiTestingPlaygroundTests.java
@@ -23,54 +23,54 @@ public class UiTestingPlaygroundTests extends BaseTest {
     }
 
     HomePage homePage=new HomePage(driver);
-//    DynamicTablePage dynamicTablePage=new DynamicTablePage(driver);
-//    OverlappedElementPage overlappedElementPage=new OverlappedElementPage(driver);
-//    SampleAppPage sampleAppPage=new SampleAppPage(driver);
-//    DynamicIdPage dynamicIdPage=new DynamicIdPage(driver);
-//
-//
-//    @Test
-//    public void dynamicTablePageTest(){
-//        homePage.goTo();
-//        waitForPageToLoad();
-//        homePage.goToDynamicTablePage();
-//        waitForPageToLoad();
-//        double cpuCellValue=dynamicTablePage.getCpuLoadValue("Chrome");
-//        double cpuLabelValue=dynamicTablePage.getCpuLabelValue();
-//        Assert.assertEquals(cpuCellValue,cpuLabelValue);
-//    }
-//
-//    @Test
-//    public void overlappedElementPageTest(){
-//        homePage.goTo();
-//        homePage.goToOverlappedElementPage();
-//        overlappedElementPage.setNameFieldValue("Test");
-//        String addedText=overlappedElementPage.getNameFieldValue();
-//        Assert.assertEquals(addedText,"Test");
-//    }
-//
-//    @Test
-//    public void sampleAppPageTest(){
-//        homePage.goTo();
-//        homePage.goToSampleAppPage();
-//        sampleAppPage.setUserName("Test");
-//        sampleAppPage.setPassWord("pwd");
-//        sampleAppPage.login();
-//        Assert.assertTrue(sampleAppPage.getLoginSuccessMessage().contains("Welcome"));
-//    }
-//
-//    @Test
-//    public void dynamicIdPage(){
-//        homePage.goTo();
-//        homePage.goToDynamicIdPage();
-//        String btnIDValueBeforeRefresh=dynamicIdPage.getBtnIDAttribute();
-//        dynamicIdPage.clickDynamicBtn();
-//        String btnIDTextAfterFirstClick=dynamicIdPage.getBtnIdText();
-//        refreshWebPage();
-//        String btnIDValueafterRefersh=dynamicIdPage.getBtnIDAttribute();
-//        dynamicIdPage.clickDynamicBtn();
-//        String btnIDTextAfterSecondClick=dynamicIdPage.getBtnIdText();
-//        Assert.assertNotEquals(btnIDValueBeforeRefresh,btnIDValueafterRefersh);
-//        Assert.assertEquals(btnIDTextAfterFirstClick,btnIDTextAfterSecondClick);
-//    }
+    DynamicTablePage dynamicTablePage=new DynamicTablePage(driver);
+    OverlappedElementPage overlappedElementPage=new OverlappedElementPage(driver);
+    SampleAppPage sampleAppPage=new SampleAppPage(driver);
+    DynamicIdPage dynamicIdPage=new DynamicIdPage(driver);
+
+
+    @Test
+    public void dynamicTablePageTest(){
+        homePage.goTo();
+        waitForPageToLoad();
+        homePage.goToDynamicTablePage();
+        waitForPageToLoad();
+        double cpuCellValue=dynamicTablePage.getCpuLoadValue("Chrome");
+        double cpuLabelValue=dynamicTablePage.getCpuLabelValue();
+        Assert.assertEquals(cpuCellValue,cpuLabelValue);
+    }
+
+    @Test
+    public void overlappedElementPageTest(){
+        homePage.goTo();
+        homePage.goToOverlappedElementPage();
+        overlappedElementPage.setNameFieldValue("Test");
+        String addedText=overlappedElementPage.getNameFieldValue();
+        Assert.assertEquals(addedText,"Test");
+    }
+
+    @Test
+    public void sampleAppPageTest(){
+        homePage.goTo();
+        homePage.goToSampleAppPage();
+        sampleAppPage.setUserName("Test");
+        sampleAppPage.setPassWord("pwd");
+        sampleAppPage.login();
+        Assert.assertTrue(sampleAppPage.getLoginSuccessMessage().contains("Welcome"));
+    }
+
+    @Test
+    public void dynamicIdPage(){
+        homePage.goTo();
+        homePage.goToDynamicIdPage();
+        String btnIDValueBeforeRefresh=dynamicIdPage.getBtnIDAttribute();
+        dynamicIdPage.clickDynamicBtn();
+        String btnIDTextAfterFirstClick=dynamicIdPage.getBtnIdText();
+        refreshWebPage();
+        String btnIDValueafterRefersh=dynamicIdPage.getBtnIDAttribute();
+        dynamicIdPage.clickDynamicBtn();
+        String btnIDTextAfterSecondClick=dynamicIdPage.getBtnIdText();
+        Assert.assertNotEquals(btnIDValueBeforeRefresh,btnIDValueafterRefersh);
+        Assert.assertEquals(btnIDTextAfterFirstClick,btnIDTextAfterSecondClick);
+    }
 }
