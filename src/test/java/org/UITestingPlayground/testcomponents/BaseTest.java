4d9ad94b3707a36f5514b23745ecc8e44f3cf1f7
Hend <hendfouda@gmail.com>
2023-01-13 16:48:57 +0200

    test

--- src/test/java/org/UITestingPlayground/testcomponents/BaseTest.java
+++ src/test/java/org/UITestingPlayground/testcomponents/BaseTest.java
@@ -3,6 +3,7 @@ package org.UITestingPlayground.testcomponents;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterClass;
@@ -26,6 +27,8 @@ public class BaseTest {
         if (driver == null) {
             switch (browserName.toLowerCase()) {
                 case "chrome":
+                    ChromeOptions options = new ChromeOptions();
+                    options.addArguments("headless");
                     WebDriverManager.chromedriver().setup();
                     driver = new ChromeDriver();
                     break;
